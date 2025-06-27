e0b44b9814fb9484a47ac56a1ca306161b6678f0
Nicole Tomazoni Pelentir <124601530+nicoletomazonipelentir@users.noreply.github.com>
2025-06-27 07:07:43 -0300

    Delete compilador_ruiva directory

--- compilador_ruiva/programa.py
+++
@@ -1,6 +0,0 @@
-# Programa iniciado
-M = 0
-for M in range(0, M<5):
-print(M)
-pass
-pass
--- compilador_ruiva/programa_compilado.py
+++
@@ -1,6 +0,0 @@
-# Programa iniciado
-M = 0  # Alex
-for M  in range( 0;  M < 5;  M):
-print(M)
-    pass
-    pass
